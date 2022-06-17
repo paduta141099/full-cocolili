b5a29c6194988e6efbdc503b1a8fc8cf55a42267
paduta141099 <54402512+paduta141099@users.noreply.github.com>
2022-06-17 12:43:51 +0700

    test

---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Prices from "./pages/Prices";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="prices" element={<Prices />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+
+
+
+app.get("/", (req, res) => {
+    res.send('hello sadas');
+})
+
+app.listen(3001, () => {
+    console.log('Running on port 3001')
+})
